742a0b1ba1871da210aea4332d97dfbf047b1e3a
Twaf Simon <73712242+Twafsimon@users.noreply.github.com>
2021-10-12 10:00:33 +0300

    Create first_python.py
    Introduction to  matplot and line plot

---
+++ first_python.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+df_can = pd.read_excel(
+    'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.xlsx',
+    sheet_name='Canada by Citizenship',
+    skiprows=range(20),
+    skipfooter=2)
+
+print('Data read into a pandas dataframe!')
+df_can.head()
